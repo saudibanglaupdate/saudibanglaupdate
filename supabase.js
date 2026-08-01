f77da43918601c08600d6f80add2fc4f24542eb0
saudibanglaupdate <saudibanglaupdate@gmail.com>
2026-08-01 09:58:45 +0300

    Initialize Supabase client with URL and key

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://dgghthghqfdgnehmosyy.supabase.co";
+
+const SUPABASE_KEY = "YOUR_PUBLISHABLE_KEY";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);const SUPABASE_KEY = "sb_publishable_GgmNhRBZDRtCXryfGqQIuQ_66EJmtoF ";
