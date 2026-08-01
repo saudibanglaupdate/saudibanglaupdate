62425df8821922f2a811bc25dc7f008d6616844a
saudibanglaupdate <saudibanglaupdate@gmail.com>
2026-08-01 17:16:10 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,8 @@
 const SUPABASE_URL = "https://dgghthghqfdgnehmosyy.supabase.co";
 
-const SUPABASE_KEY = "YOUR_PUBLISHABLE_KEY";
+const SUPABASE_KEY = "sb_publishable_GgmNhRBZDRtCXryfGqQIuQ_66EJmtoF";
 
 const supabase = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
-);const SUPABASE_KEY = "sb_publishable_GgmNhRBZDRtCXryfGqQIuQ_66EJmtoF ";
+);
